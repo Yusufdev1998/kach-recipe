ccf1e0ae5a756e3b9a28d80bc570f33398d591ec
Yusufdev1998 <yusufdev1998@gmail.com>
2023-08-31 17:01:10 +0500

    added

--- dist/app.js
+++ dist/app.js
@@ -4,12 +4,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const recipes_route_1 = __importDefault(require("./routes/recipes.route"));
 const user_route_1 = __importDefault(require("./routes/user.route"));
 const auth_middleware_1 = __importDefault(require("./middleware/auth.middleware"));
 class App {
     constructor() {
         this.appServer = (0, express_1.default)();
+        this.appServer.use((0, cors_1.default)());
         this.appServer.use(express_1.default.json());
         this.setRoutes();
     }
